765ebce6c89e395e2fb6557d712bef837b12109c
Penguin <son@vdburg.site>
2024-12-18 10:56:34 +0100

    begin aan detail pagina

--- detail.php
+++ detail.php
@@ -1,7 +1,17 @@
 <?php
 
 // Get the data from the JSON file
-$projectData = json_decode(file_get_contents('projectData.json'), true);
+$allProjectData = json_decode(file_get_contents('projectData.json'), true);
+$projectID = $_GET['id'];
+
+foreach ($allProjectData as $category) {
+    foreach ($category['projects'] as $project) {
+        if ($project['id'] == $projectID) {
+            $projectData = $project;
+            break 2;
+        }
+    }
+}
 
 ?>
 
@@ -19,26 +29,51 @@ $projectData = json_decode(file_get_contents('projectData.json'), true);
     />
 </head>
 <body class="bg-dark text-light">
-            <!-- NAVIGATION BAR -->
-            <nav class="navbar navbar-expand-lg text-light bg-dark border-bottom sticky-top p-0 m-0">
-            <div class="container-fluid d-flex justify-content-between">
-                <!-- Title -->
-                <p class="navbar-brand text-light p-0 m-0"><span class="fs-1 pe-2">Modino</span> Bart van der Burg</p>
+    <!-- NAVIGATION BAR -->
+    <nav class="navbar navbar-expand-lg text-light bg-dark border-bottom sticky-top p-0 m-0">
+        <div class="container-fluid d-flex justify-content-between">
+            <!-- Title -->
+            <p class="navbar-brand text-light p-0 m-0"><span class="fs-1 pe-2">Modino</span> Bart van der Burg</p>
 
-                <!-- Navigation -->
-                <div class="d-flex flex-row">
-                    <a class="btn btn-outline-light fs-6 me-3" href="#about">Over mij</a>
-
-                    <?php
-                    foreach ($projectData as $category) {
-                        echo '<a href="#' . $category['category'] . '" class="btn btn-outline-light fs-6 me-3">' . $category['category'] . '</a>';
-                    }
-                    ?>
-
-                    <a class="btn btn-outline-light fs-6" href="#contact">Contact</a>
-                </div>
+            <!-- Navigation -->
+            <div class="d-flex flex-row">
+                <a class="btn btn-outline-light fs-6" href="index.php">Home</a>
             </div>
-        </nav>
+        </div>
+    </nav>
+
+    <!-- MAIN CONTENT -->
+    <div class="container">
+        <!-- TITLE -->
+        <div class="row text-center">
+            <h1 class="mt-5" style="font-size: 70px"><?= $projectData['name'] ?></h1>
+            <h2><?= $projectData['headerLine'] ?></h2>
+        </div>
+
+        <div class="row d-flex justify-content-center">
+            <!-- DESCRIPTION -->
+            <div class="col-lg-8 m-3 p-3 border rounded d-flex justify-content-between flex-row">
+                <p class="col-lg-6"><?= $projectData['description'] ?></p> 
+
+                <!-- MAIN IMAGE -->
+                <img src=<?= $projectData['mainPicture'] ?> alt="Main Picture" class="col-lg-6 img-fluid rounded">
+            </div>
+        </div>
+
+        <!-- IMAGES -->
+        <div class="row mt-5 d-flex justify-content-center">
+            <div class="col-lg-8 border rounded d-flex justify-content-center flex-wrap">
+            <?php foreach ($projectData['pictures'] as $picture) { ?>
+                <div class="col-lg-5 d-flex align-items-center m-3"><img src=<?= $picture ?> alt="Picture" class="img-fluid rounded m-3"></div>
+            <?php } ?>
+            </div>
+        </div>
+    </div>
+
+    <!-- FOOTER -->
+    <div class="border-top mt-5 p-5 text-center">
+        <p>Placeholder tekst. Kan vervangen worden met een footer sectie.</p>
+    </div>
 
     <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
