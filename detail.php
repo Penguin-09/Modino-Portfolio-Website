5b926c6074fcdb9ee88bd6fa6effc56d41c9b511
BvdB020 <61408252+BvdB020@users.noreply.github.com>
2026-07-08 14:09:43 +0200

    Add files via upload

--- detail.php
+++ detail.php
@@ -20,7 +20,7 @@ foreach ($allProjectData as $category) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detail</title>
+    <title>Modino</title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         rel="stylesheet"
